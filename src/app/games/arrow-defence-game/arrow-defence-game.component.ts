0882a4f60a7f7f0972b9450170063d4ab010eb2d
Sairam-Reddy <sairamreddyg33@gmail.com>
2022-01-07 13:35:33 +0530

    1 file modified

--- src/app/games/arrow-defence-game/arrow-defence-game.component.ts
+++ src/app/games/arrow-defence-game/arrow-defence-game.component.ts
@@ -193,7 +193,10 @@ export class ArrowDefenceGameComponent implements AfterViewInit, OnDestroy {
     }
 
     this.setupEventListeners();
-    this.onResize();
+    this.animateView();
+    // setTimeout(() => {
+    //   this.animateView();
+    // }, 1000 / 60);
   }
 
   private setupEventListeners(): void {
@@ -241,6 +244,202 @@ export class ArrowDefenceGameComponent implements AfterViewInit, OnDestroy {
         this.motchend(e.touches[0]);
       },
       false
+    );
+  }
+
+  private animateView(): void {
+    this.requestAnimFrame();
+
+    if (this.shake) {
+      var trax = Math.random() * 60 - 30;
+      var tray = Math.random() * 60 - 30;
+      this.ctx.translate(trax, tray);
+    }
+    this.fpscounter();
+    this.ctx.clearRect(
+      0,
+      0,
+      this.canvasElement.width,
+      this.canvasElement.height
+    );
+    this.enginestep();
+
+    // unused code
+    // this.ctx.fillStyle = '#8e44ad';
+    // this.ctx.font = '24px arial';
+
+    // this.ctx.textAlign = 'left';
+    // this.ctx.fillText(this.thisfps.toString(), 20, 50);
+    // this.smoothfps += (this.thisfps - this.smoothfps) / 100;
+    // this.ctx.fillText(this.cold[0].slice(1, 6), 20, 80);
+    // this.ctx.beginPath();
+    // this.ctx.arc(this.pointer.x, this.pointer.y, 50, 0, Math.PI * 2, false);
+    // this.ctx.closePath();
+    // this.ctx.fill();
+
+    if (this.shake) {
+      this.ctx.translate(-trax, -tray);
+      this.shaket++;
+      if (this.shaket > 20) {
+        this.shaket = 0;
+        this.shake = false;
+      }
+    }
+  }
+
+  private enginestep() {
+    this.ctx.drawImage(this.backroundImage, 0, 0);
+    if (!this.ai && this.ait < Date.now() - 3000) {
+      this.ai = true;
+    }
+    this.btm++;
+    if (this.btm > 8) {
+      this.btm = 0;
+      this.bullets.push(new Bullet(this.stage, this.angle));
+    }
+
+    for (var i = 0; i < this.bullets.length; i++) {
+      this.bullets[i].x -= Math.sin(this.bullets[i].r) * 20;
+      this.bullets[i].y -= Math.cos(this.bullets[i].r) * 20;
+      this.drawArrow(
+        this.bullets[i].x + Math.sin(this.bullets[i].r) * 50,
+        this.bullets[i].y + Math.cos(this.bullets[i].r) * 50,
+        this.bullets[i].x,
+        this.bullets[i].y,
+        8,
+        8,
+        '#2980b9'
+      );
+      if (this.bullets[i].x < -100 || this.bullets[i].x > this.stage.w + 100) {
+        this.bullets.splice(i, 1);
+      }
+      if (this.bullets[i].y < -100 || this.bullets[i].y > this.stage.h + 100) {
+        this.bullets.splice(i, 1);
+      }
+    }
+
+    for (var i = 0; i < this.enemies.length; i++) {
+      this.enemies[i].x += Math.sin(this.enemies[i].r) * 3;
+      this.enemies[i].y += Math.cos(this.enemies[i].r) * 3;
+      this.drawArrow(
+        this.enemies[i].x - Math.sin(this.enemies[i].r) * 100,
+        this.enemies[i].y - Math.cos(this.enemies[i].r) * 100,
+        this.enemies[i].x,
+        this.enemies[i].y,
+        15,
+        15,
+        '#c0392b'
+      );
+      if (this.enemies[i].y > this.stage.h) {
+        this.enemies[i] = new Enemy(this.stage);
+        this.explosions.push(new Explosion(this.stage.w / 2, this.stage.h, 2));
+        this.shake = true;
+        this.shaket = 0;
+      }
+      for (var b = 0; b < this.bullets.length; b++) {
+        var dx = this.enemies[i].x - this.bullets[b].x;
+        var dy = this.enemies[i].y - this.bullets[b].y;
+        var dis = dx * dx + dy * dy;
+        if (dis < 20 * 20) {
+          this.explosions.push(
+            new Explosion(this.enemies[i].x, this.enemies[i].y, 1)
+          );
+          this.enemies[i] = new Enemy(this.stage);
+          this.bullets.splice(b, 1);
+        }
+      }
+    }
+
+    if (this.ai) {
+      for (var l = 0; l < this.enemies.length; l++) {
+        var dx = this.enemies[l].x - this.stage.w / 2;
+        var dy = this.enemies[l].y - this.stage.h;
+        var dis = Math.floor(Math.sqrt(dx * dx + dy * dy));
+        var val1 = 100000 + dis;
+        var val2 = 1000 + l;
+        this.cold[l] = val1 + 'x' + val2;
+      }
+
+      this.cold.sort();
+      this.eturn = parseInt(this.cold[0].slice(8, 11));
+      if (parseInt(this.cold[0].slice(1, 6)) < 800) {
+        this.angle += (this.enemies[this.eturn].r - this.angle) / 8;
+      }
+    } else {
+      var dx = this.pointer.x - this.stage.w / 2;
+      var dy = this.pointer.y - this.stage.h;
+      this.angle = Math.atan(dx / dy);
+    }
+
+    this.drawArrow(
+      this.stage.w / 2,
+      this.stage.h,
+      this.stage.w / 2 - Math.sin(this.angle) * 150,
+      this.stage.h - Math.cos(this.angle) * 150,
+      30,
+      20,
+      '#2c3e50'
+    );
+
+    for (var e = 0; e < this.explosions.length; e++) {
+      if (this.explosions[e].ty == 1) {
+        var myimg = this.explodeImage;
+        this.ctx.globalAlpha = 1 - this.explosions[e].t / this.stage.h;
+        this.ctx.drawImage(
+          myimg,
+          this.explosions[e].x - this.explosions[e].t / 2,
+          this.explosions[e].y - this.explosions[e].t / 2,
+          (this.explosions[e].t * this.stage.w) / this.stage.h,
+          this.explosions[e].t
+        );
+        this.ctx.globalAlpha = 1;
+      } else {
+        var myimg = this.explodebImage;
+        this.ctx.globalAlpha = 1 - this.explosions[e].t / this.stage.h;
+        this.ctx.drawImage(
+          myimg,
+          this.explosions[e].x -
+            (this.explosions[e].t * this.stage.w) / this.stage.h / 2,
+          this.stage.h - this.explosions[e].t,
+          (this.explosions[e].t * this.stage.w) / this.stage.h,
+          this.explosions[e].t
+        );
+        this.ctx.globalAlpha = 1;
+      }
+    }
+
+    for (var e = 0; e < this.explosions.length; e++) {
+      this.explosions[e].t += 20;
+      if (this.explosions[e].t > this.stage.h) {
+        this.explosions.splice(e, 1);
+      }
+    }
+  }
+
+  private fpscounter() {
+    this.timebomb++;
+    if (!this.lastCalledTime) {
+      this.lastCalledTime = Date.now();
+      return;
+    }
+    var delta = (Date.now() - this.lastCalledTime) / 1000;
+    this.lastCalledTime = Date.now();
+    var fps = 1 / delta;
+    this.fpcount++;
+    this.fpall += fps;
+    if (this.timebomb > 30) {
+      this.thisfps = ((this.fpall / this.fpcount) * 10) / 10;
+      this.fpcount = 0;
+      this.fpall = 0;
+      this.timebomb = 0;
+    }
+  }
+
+  private requestAnimFrame() {
+    window.requestAnimationFrame(() =>
+      setTimeout(() => {
+        this.animateView();
+      }, 1000 / 60)
     );
   }
 
