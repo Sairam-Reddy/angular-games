821aee10ea35a2f7f20f5961bb3b0f3d2de918e2
Sairam-Reddy <sairamreddyg33@gmail.com>
2022-01-07 14:30:31 +0530

    2 files modified

--- src/app/games/arrow-defence-game/models/bullet.model.ts
+++ src/app/games/arrow-defence-game/models/bullet.model.ts
@@ -1,8 +1,10 @@
+import { Stage } from './stage.model';
+
 export class Bullet {
   public x: number;
   public y: number;
   public r: number;
-  public constructor(public stage: any, public angle: any) {
+  public constructor(public stage: Stage, public angle: number) {
     this.x = stage.w / 2 - Math.sin(angle) * 150;
     this.y = stage.h - Math.cos(angle) * 150;
     this.r = angle;
--- src/app/games/arrow-defence-game/models/enemy.model.ts
+++ src/app/games/arrow-defence-game/models/enemy.model.ts
@@ -1,10 +1,12 @@
+import { Stage } from './stage.model';
+
 export class Enemy {
   public r: number;
   public dis: number;
   public x: number;
   public y: number;
 
-  public constructor(public stage: any) {
+  public constructor(public stage: Stage) {
     this.r = (Math.random() * Math.PI) / (2.5 / 2) - Math.PI / 2.5;
     this.dis = Math.random() * 1280 + 720;
     this.x = stage.w / 2 - Math.sin(this.r) * this.dis;
