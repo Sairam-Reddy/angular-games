bfbe608e6d327ca22bd6665d75c97a9e177570e9
Sairam-Reddy <sairamreddyg33@gmail.com>
2022-01-14 17:59:15 +0530

    1 file modified

--- src/app/games/pacman-game/models/pacman.model.ts
+++ src/app/games/pacman-game/models/pacman.model.ts
@@ -363,9 +363,9 @@ export class Pacman {
   }
 
   public loaded() {
-    if (this.state === WAITING) {
-      this.dialog('Press N or tap to start a New game');
-    }
+    // if (this.state === WAITING) {
+    //   this.dialog('Press N or tap to start a New game');
+    // }
     document.addEventListener('keydown', this.keyDown.bind(this), true);
     document.addEventListener('keypress', this.keyPress.bind(this), true);
 
