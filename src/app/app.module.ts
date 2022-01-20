abc600623ec0998e2d84e1f1006c1d7efd1680d1
Sairam-Reddy <sairamreddyg33@gmail.com>
2022-01-20 16:34:27 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,25 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
 import { HomeComponent } from './home/home.component';
+import { TicTacToeGameModule } from './games/tic-tac-toe-game/tic-tac-toe-game.module';
+import { ArrowDefenceGameModule } from './games/arrow-defence-game/arrow-defence-game.module';
+import { PinballGameModule } from './games/pinball-game/pinball-game.module';
+import { AsteroidGameModule } from './games/asteroid-game/asteroid-game.module';
+import { HighwayRaceGameModule } from './games/highway-race-game/highway-race-game.module';
+import { PacmanGameModule } from './games/pacman-game/pacman-game.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    TicTacToeGameModule,
+    ArrowDefenceGameModule,
+    PacmanGameModule,
+    PinballGameModule,
+    AsteroidGameModule,
+    HighwayRaceGameModule,
+  ],
   declarations: [AppComponent, HomeComponent],
   bootstrap: [AppComponent],
 })
