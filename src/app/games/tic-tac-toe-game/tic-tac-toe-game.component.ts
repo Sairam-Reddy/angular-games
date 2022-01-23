3aa8d3fa5974ce7f2a50bb141403407684b534a6
Sairam-Reddy <sairamreddyg33@gmail.com>
2022-01-23 10:29:34 +0530

    updated computer move sound

--- src/app/games/tic-tac-toe-game/tic-tac-toe-game.component.ts
+++ src/app/games/tic-tac-toe-game/tic-tac-toe-game.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tic-tac-toe-game',
   templateUrl: './tic-tac-toe-game.component.html',
   styleUrls: ['./tic-tac-toe-game.component.scss'],
 })
-export class TicTacToeGameComponent {
+export class TicTacToeGameComponent implements OnInit {
   private moveTicTacToeAudio: HTMLAudioElement;
   private winTicTacToeAudio: HTMLAudioElement;
 
-  public computerMove(): void {}
+  public ngOnInit(): void {
+    this.moveTicTacToeAudio = new Audio();
+    this.moveTicTacToeAudio.src =
+      'https://stackblitz.com/files/angular-ivy-kbykxm/github/Sairam-Reddy/angular-games/master/src/assets/audio/Move-Tic-Tac-Toe.mp3';
+    this.moveTicTacToeAudio.load();
+
+    this.winTicTacToeAudio = new Audio();
+    this.winTicTacToeAudio.src =
+      'https://stackblitz.com/files/angular-ivy-kbykxm/github/Sairam-Reddy/angular-games/master/src/assets/audio/Winning-Tic-Tac-Toe.mp3';
+    this.winTicTacToeAudio.load();
+  }
+
+  public computerMove(): void {
+    this.moveTicTacToeAudio.play
+  }
 }
