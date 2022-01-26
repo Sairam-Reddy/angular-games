e603551581e2ad7fb99b5ee433aedb2415b3d9c5
Sairam-Reddy <sairamreddyg33@gmail.com>
2022-01-26 10:54:55 +0530

    updated module

---
+++ src/app/games/shooting-hoops-game/shooting-hoops-game.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ShootingHoopsGameComponent } from './shooting-hoops-game.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ShootingHoopsGameComponent,
+  },
+];
+
+@NgModule({
+  imports: [CommonModule, FormsModule, RouterModule.forChild(routes)],
+  declarations: [ShootingHoopsGameComponent],
+  exports: [ShootingHoopsGameComponent],
+})
+export class ShootingHoGameModule {}
